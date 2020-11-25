04aebdeac64ba4d142f9a0d246eb326dbc1b637e
tomobre <71527754+tomobre@users.noreply.github.com>
2020-11-25 19:44:10 -0300

    Delete recetario.js

--- recetario.js
+++
@@ -1,9 +0,0 @@
-let ingred1 = prompt("Ingrese  primer ingrediente para receta");
-let ingred2 = prompt("Ingrese segundo  ingrediente para  receta");
-let ingred3 = prompt("Ingrese tercer  ingrediente para receta");
-
-document.write(`<ul>
-<li>${ingred1}</li>
-<li>${ingred2}</li >
-<li>${ingred3}</li>
-</ul>`);
